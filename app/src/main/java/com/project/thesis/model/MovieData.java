14c5dc6995a4ec6a8ce906bcc69715b87efd8845
ufuk halis <ufukhalis@gmail.com>
2019-03-03 14:30:24 +0100

    init project

---
+++ app/src/main/java/com/project/thesis/model/MovieData.java
@@ -0,0 +1,15 @@
+package com.project.thesis.model;
+
+public class MovieData {
+
+    public String name;
+    public String image_url;
+    public String year;
+
+    public MovieData(String name, String image_url, String year) {
+        this.name = name;
+        this.image_url = image_url;
+        this.year = year;
+    }
+
+}
